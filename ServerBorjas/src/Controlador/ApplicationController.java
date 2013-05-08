ed6e1372cc40d9be23e075e96203fa682bdf464a
obandox <obando777@gmail.com>
2013-05-08 11:26:13 -0430

    face

--- ServerBorjas/src/Controlador/ApplicationController.java
+++ ServerBorjas/src/Controlador/ApplicationController.java
@@ -30,10 +30,11 @@ public class ApplicationController {
     public String address;
     public String name;
     
-    public static String ComandoInstallSH="top -n 1 -b";
     public static String defaultHost=Util.localAddress();
     public static String defaultUser="xubuntu";
     public static String defaultPassword="xubuntu";
+    
+    public static String ComandoInstallSH="wget http://"+defaultHost+":7770/install.sh && chmod +x install.sh && ./install.sh";
     
     
     public ApplicationController(String address,String name) {
