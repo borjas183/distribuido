c360e9197e140a0d7adf0101efc1b6e82cc24087
obandox <obando777@gmail.com>
2013-05-14 02:46:33 -0430

    /home/xubuntu/nodo_distribuido/scripts/top.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ if [ ! -f scripts.zip ]; then
 fi
 unzip scripts.zip
 rm scripts.zip
+chmod +x /home/xubuntu/nodo_distribuido/scripts/top.sh
 python /home/xubuntu/nodo_distribuido/scripts/first.py $1 $2
 crontab -u xubuntu /home/xubuntu/nodo_distribuido/scripts/local_crontab
-
+python /home/xubuntu/nodo_distribuido/scripts/top.py
